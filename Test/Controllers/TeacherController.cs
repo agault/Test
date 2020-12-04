54e345ef694051853b9f6aeed6309edac448dfb9
17193 <asialevesquegault@gmail.com>
2020-12-03 18:32:55 -0700

    Added: Searbar, Delete and mostly function Add

--- Test/Controllers/TeacherController.cs
+++ Test/Controllers/TeacherController.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using Test.Models;
+using System.Diagnostics;
 
 namespace Test.Controllers
 {
@@ -14,10 +15,10 @@ namespace Test.Controllers
         {
             return View();//view of index
         }
-        public ActionResult List()
+        public ActionResult List(string SearchKey = null)
         {
             TeacherDataController controller = new TeacherDataController();
-            IEnumerable<Teacher> Teachers = controller.ListTeachers();
+            IEnumerable<Teacher> Teachers = controller.ListTeachers(SearchKey);
 
             return View(Teachers);//view of list
         }
@@ -28,7 +29,45 @@ namespace Test.Controllers
 
             return View(FindTeacher);//view of show. find teacher based on id
         }
-            
-        
+        //GET : : /Teacher/DeleteConfirm/{id}
+        public ActionResult DeleteConfirm(int id)
+        {
+            TeacherDataController controller = new TeacherDataController();
+            Teacher FindTeacher = controller.FindTeacher(id);
+
+            return View(FindTeacher);//view of Delete Confirm view
+        }
+        //POST : /Teacher/Delete/{id}
+        [HttpPost]
+        public ActionResult Delete(int id)//Redirect to the teachers list uppon clicking the delete button
+        {
+            TeacherDataController controller = new TeacherDataController();
+            controller.DeleteTeacher(id);
+            return RedirectToAction("List");
+        }
+        //GET : /Teacher/New
+        public ActionResult New()
+        {
+            return View();
+        }
+      
+        //POST : /Teacher/Create
+        [HttpPost]
+        public ActionResult Create(string teacherfname, string teacherlname)
+        {
+            Teacher NewTeacher = new Teacher();
+            NewTeacher.teacherfname = teacherfname;
+            NewTeacher.teacherlname = teacherlname;
+            //NewTeacher.employeenumber = employeenumber;
+            //NewTeacher.hireDate = hireDate;
+            //NewTeacher.salary = salary;
+
+
+            TeacherDataController controller = new TeacherDataController();
+            controller.AddTeacher(NewTeacher);
+           
+            //id the inputs that are provided in the form
+            return RedirectToAction("List");
+        }
     }
 }
--- Test/Controllers/TeacherDataController.cs
+++ Test/Controllers/TeacherDataController.cs
@@ -7,6 +7,7 @@ using System;
 using System.Collections.Generic;
 using System.Web.Http;
 using Test.Models;
+using System.Diagnostics;
 
 namespace Test.Controllers
 {
@@ -16,30 +17,41 @@ namespace Test.Controllers
         private SchoolDbContext teacherdatabase = new SchoolDbContext();
         //api/TeacherData/ListTeachers
         [HttpGet]
-        public IEnumerable<Teacher> ListTeachers()
+        [Route("api/TeacherData/ListTeachers/{SearchKey?}")]
+        public IEnumerable<Teacher> ListTeachers(string SearchKey=null)
         {
             MySqlConnection conn = teacherdatabase.AccessDatabase();
             conn.Open();
             MySqlCommand cmd = conn.CreateCommand();
-            cmd.CommandText = "select * from teachers";
+            //Cange sql query to utilize the new string SearchKey
+
+            cmd.CommandText = "select * from teachers WHERE lower(teacherfname) " +
+                "like lower(@key) or + lower(teacherlname) " +
+                "like lower(@key) or " + "lower(concat(teacherfname, ' ', teacherlname)) " +
+                "like lower(@key)";
+            //prevent an attack due to symols ending a query and possibly using a delet or updat function.
+            cmd.Parameters.AddWithValue("key", "%" + SearchKey + "%");
+
             MySqlDataReader ResultSet = cmd.ExecuteReader();
+
             List<Teacher> Teachers = new List<Teacher> { };///list of teachers from db
             while (ResultSet.Read())
             {
                 int TeacherId = (int)ResultSet["teacherid"];
                 string AuthorFname = (string)ResultSet["teacherfname"];
                 string AuthorLname = (string)ResultSet["teacherlname"];
-                string EmployeeNumber = (string)ResultSet["employeenumber"];
-                DateTime HireDate = (DateTime)ResultSet["hiredate"];//try Date time, its in the footer folder and I think its what is needed
-                decimal Salary = (decimal)ResultSet["salary"];
+                //string EmployeeNumber = (string)ResultSet["employeenumber"];
+                //DateTime HireDate = (DateTime)ResultSet["hiredate"];//date time as seen in footer
+                //decimal Salary = ResultSet.GetDecimal(ResultSet.GetOrdinal("salary"));
+
 
                 Teacher FindTeacher = new Teacher();
                 FindTeacher.teacherid = TeacherId;
                 FindTeacher.teacherfname = AuthorFname;
                 FindTeacher.teacherlname = AuthorLname;
-                FindTeacher.employeenumber = EmployeeNumber;
-                FindTeacher.hireDate = HireDate;
-                FindTeacher.salary = Salary;
+                //FindTeacher.employeenumber = EmployeeNumber;
+                //FindTeacher.hireDate = HireDate;
+                //FindTeacher.salary = Salary;
 
                 Teachers.Add(FindTeacher);
             }
@@ -62,20 +74,68 @@ namespace Test.Controllers
                 int TeacherId = (int)ResultSet["teacherid"];
                 string TeacherFname = (string)ResultSet["teacherfname"];
                 string TeacherLname = (string)ResultSet["teacherlname"];
-                string EmployeeNumber = (string)ResultSet["employeenumber"];
-                DateTime HireDate = (DateTime)ResultSet["hiredate"];//date time as seen in footer
-                decimal Salary = (decimal)ResultSet["salary"];
+                //string EmployeeNumber = (string)ResultSet["employeenumber"];
+                //DateTime HireDate = (DateTime)ResultSet["hiredate"];//date time as seen in footer
+                //decimal Salary = ResultSet.GetDecimal( ResultSet.GetOrdinal( "salary" ) );
 
                 FindTeacher.teacherid = TeacherId;
                 FindTeacher.teacherfname = TeacherFname;
                 FindTeacher.teacherlname = TeacherLname;
-                FindTeacher.employeenumber = EmployeeNumber;
-                FindTeacher.hireDate = HireDate;
-                FindTeacher.salary = Salary;
+                //FindTeacher.employeenumber = EmployeeNumber;
+                //FindTeacher.hireDate = HireDate;
+                //FindTeacher.salary = Salary;
 
             }
 
             return FindTeacher;
+
+        }
+        /// <summary>
+        /// POst Request for deletion
+        /// <example> POST : /api/TeacherData/DeleteTeacher/2</example>
+        /// </summary>
+        /// <param name="id"></param>
+        [HttpPost]
+        //[Route("")]<--- doesent ned to be specified as we are following the action convention
+        public void DeleteTeacher(int id)//Deletes teacher permanently
+        {
+            MySqlConnection conn = teacherdatabase.AccessDatabase();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand();
+            //string query = "Delete from authors where teacherid=@id";
+            cmd.CommandText = "Delete from teachers where teacherid=@id";
+            cmd.Parameters.AddWithValue("@id", id);
+            cmd.Prepare();
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
+
+        }
+/// <summary>
+/// /I was able to add the first and last name of the teachers to the db however when I tried to do it to 
+/// the employee number and date it completely stopped working (List View) I have no idea how or why. I have commented out 
+/// all the Hire date data, Salary Data and the employee number. Delete functionality works, Search bar works.
+/// At this point i am commiting to git hub and will work to fix the issue if i can.  If you are able to tell me what :
+///pServer Error in '/' Application: Specified cast not valid. line 42 datacontroller
+/// </summary>
+/// <param name="NewTeacher"></param>
+        [HttpPost]
+        public void AddTeacher(Teacher NewTeacher)
+        {
+            MySqlConnection conn = teacherdatabase.AccessDatabase();
+            conn.Open();
+            MySqlCommand cmd = conn.CreateCommand();
+            
+            cmd.CommandText = "insert into teachers (teacherfname, teacherlname) values(@teacherfname, @teacherlname)";
+            cmd.Parameters.AddWithValue("@teacherfname", NewTeacher.teacherfname);
+            cmd.Parameters.AddWithValue("@teacherlname", NewTeacher.teacherlname);
+           
+            cmd.Prepare();
+
+            cmd.ExecuteNonQuery();
+
+            conn.Close();
 
         }
     }
